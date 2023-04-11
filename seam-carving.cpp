b2852728588ea88b902d614b791cd3b86246a679
Lucas Leschynski <lucas.leschynski@gmail.com>
2023-04-11 23:40:55 +0100

    Fixed mat->vector bug

--- seam-carving.cpp
+++ seam-carving.cpp
@@ -9,16 +9,16 @@
 using namespace std;
 using namespace cv;
 
-Mat convert_to_edges(Mat src) {
-    int ddepth = CV_32F;
-    Mat output, grad_x, grad_y;
+Mat convert_to_edges(Mat& src) {
+    int ddepth = CV_16S;
+    Mat output, gray, grad_x, grad_y;
 
-    GaussianBlur(src, output, Size(3, 3), 0);
+    GaussianBlur(src, gray, Size(3, 3), 0);
     // output = src.clone();
-    cvtColor(output, output, COLOR_BGR2GRAY);
+    cvtColor(gray, output, COLOR_BGR2GRAY);
 
-    Sobel(output, grad_x, ddepth, 1, 0, 3);//, 0.01);  // Sobel for X direction
-    Sobel(output, grad_y, ddepth, 0, 1, 3);//, 0.01);  // Sobel for Y direction
+    Sobel(output, grad_x, ddepth, 1, 0, 3);  // Sobel for X direction
+    Sobel(output, grad_y, ddepth, 0, 1, 3);  // Sobel for Y direction
 
     // Scale
     convertScaleAbs(grad_x, grad_x);
@@ -29,7 +29,7 @@ Mat convert_to_edges(Mat src) {
     return output;
 }
 
-void mat_to_vector(const Mat& mat, vector<vector<int>> dest) {
+void mat_to_vector(Mat mat, vector<vector<int>>& dest) {
     vector<vector<int>> vec(mat.rows, vector<int>(mat.cols));
     for (int i = 0; i < mat.rows; i++) {
         for (int j = 0; j < mat.cols; j++) {
@@ -43,7 +43,7 @@ void print_2d_vec(const vector<vector<int>>& vec) {
     for (const auto& row : vec) {
         cout << "[";
         for (const auto& element : row) {
-            cout << element << ", ";
+            cout << element << ",";
         }
         cout << "]";
         cout << "\n" << endl;
@@ -53,25 +53,18 @@ void print_2d_vec(const vector<vector<int>>& vec) {
 int main(int argc, char** argv) {
     string image_path = "images/shapes.jpg";
     Mat img = imread(image_path);
-    Mat gray;
-    cvtColor(img, gray, COLOR_BGR2GRAY);
-
     Mat sobel, edges;
 
-    // resize(gray, gray, Size(), 0.01, 0.01);
-
     edges = convert_to_edges(img);
+
+    resize(edges, edges, Size(), 0.1, 0.1);
 
     vector<vector<int>> edge_vec;
     mat_to_vector(edges, edge_vec);
 
-    // print_2d_vec(test);
 
-    // Point2f P(5, 1);
-    // Scalar pixel = img.at<uchar>(P);
-    // cout << "Point (2D) = " << pixel << endl << endl;
+
     imshow("original", img);
-
     imshow("edges", edges);
 
     waitKey(0);
