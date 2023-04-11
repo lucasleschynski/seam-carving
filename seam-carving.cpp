a5d5f73160121e5327d9c57c4621b19ee3ee69a9
Lucas Leschynski <lucas.leschynski@gmail.com>
2023-04-11 23:51:38 +0100

    Cleaned up

--- seam-carving.cpp
+++ seam-carving.cpp
@@ -11,7 +11,7 @@ using namespace cv;
 
 Mat convert_to_edges(Mat& src) {
     int ddepth = CV_16S;
-    Mat output, gray, grad_x, grad_y;
+    Mat output, gray, grad_x, grad_y, energy_image;
 
     GaussianBlur(src, gray, Size(3, 3), 0);
     // output = src.clone();
@@ -20,12 +20,17 @@ Mat convert_to_edges(Mat& src) {
     Sobel(output, grad_x, ddepth, 1, 0, 3);  // Sobel for X direction
     Sobel(output, grad_y, ddepth, 0, 1, 3);  // Sobel for Y direction
 
+    // Scharr(output, grad_x, ddepth, 1, 0);
+    // Scharr(output, grad_y, ddepth, 0, 1);
+
     // Scale
     convertScaleAbs(grad_x, grad_x);
     convertScaleAbs(grad_y, grad_y);
 
     // Combine X and Y gradients
     addWeighted(grad_x, 0.5, grad_y, 0.5, 0, output);
+
+    output.convertTo(energy_image, CV_64F, 1.0/255.0);
     return output;
 }
 
@@ -51,18 +56,14 @@ void print_2d_vec(const vector<vector<int>>& vec) {
 }
 
 int main(int argc, char** argv) {
-    string image_path = "images/shapes.jpg";
+    string image_path = "images/bikes.jpg";
     Mat img = imread(image_path);
     Mat sobel, edges;
 
     edges = convert_to_edges(img);
 
-    resize(edges, edges, Size(), 0.1, 0.1);
-
     vector<vector<int>> edge_vec;
     mat_to_vector(edges, edge_vec);
-
-
 
     imshow("original", img);
     imshow("edges", edges);
