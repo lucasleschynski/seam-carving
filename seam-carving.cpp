e99be3ea6cfc8f01ce24a65c3137d7f712fb598f
Lucas Leschynski <lucas.leschynski@gmail.com>
2023-04-11 22:42:45 +0100

    Added cmake

--- seam-carving.cpp
+++ seam-carving.cpp
@@ -8,40 +8,45 @@
 using namespace std;
 using namespace cv;
 
+Mat convert_to_edges(Mat src) {
+    int ddepth = CV_16S;
+    Mat output, grad_x, grad_y;
+
+    // GaussianBlur(src, output, Size(3, 3), 0);
+    output = src.clone();
+    cvtColor(output, output, COLOR_BGR2GRAY);
+
+    Sobel(output, grad_x, ddepth, 1, 0, 3);//, 0.01);  // Sobel for X direction
+    Sobel(output, grad_y, ddepth, 0, 1, 3);//, 0.01);  // Sobel for Y direction
+
+    // Scale
+    convertScaleAbs(grad_x, grad_x);
+    convertScaleAbs(grad_y, grad_y);
+
+    // Combine X and Y gradients
+    addWeighted(grad_x, 0.5, grad_y, 0.5, 0, output);
+    return output;
+}
+
 int main(int argc, char** argv) {
-    string image_path = "images/golf.jpg";
+    string image_path = "images/shapes.jpg";
     Mat img = imread(image_path);
-    Mat imgGray, imgBlur1, imgBlur2;
+    Mat sobel, edges;
 
-    // cvtColor(img,imgGray,COLOR_BGR2RGB);
+    resize(img, img, Size(), 0.5, 0.5);
 
-    GaussianBlur(img,imgBlur1,Size(25,25),5,5);
-    GaussianBlur(img,imgBlur2,Size(5,5),45,45);
+    edges = convert_to_edges(img);
+
 
     Point2f P(5, 1);
-
     Scalar pixel = img.at<uchar>(P);
     cout << "Point (2D) = " << pixel << endl << endl;
-    //imshow("Test1", imgGray);
+
+    imshow("sobel", edges);
 
     // imshow("Test1", imgBlur1);
     // imshow("Test2", imgBlur2);
 
     waitKey(0);
 }
-
-// int main(int argc, char** argv) {
-//     string vid_path = "images/earth.mp4";
-//     VideoCapture cap(1);
-//     Mat img;
-
-//     while(true) {
-//         cap.read(img);
-//         imshow("Image", img);
-//         waitKey(1);
-//     }
-
-//     // imshow("Test", img);
-//     // waitKey(0);
-// }
 
